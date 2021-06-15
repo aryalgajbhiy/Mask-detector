f691f278aa6d97222ba5efe92d1669a209b42aa9
aryalgajbhiy <85826792+aryalgajbhiy@users.noreply.github.com>
2021-06-16 00:11:36 +0530

    Update train_mask_detector.py

--- glimpse2.9.5/train_mask_detector.py
+++ glimpse2.9.5/train_mask_detector.py
@@ -1,5 +1,5 @@
-# import the necessary packages
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+#CNN,  MobileNet
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.layers import AveragePooling2D
 from tensorflow.keras.layers import Dropout
@@ -9,9 +9,12 @@ from tensorflow.keras.layers import Input
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+#for converting image to array
 from tensorflow.keras.preprocessing.image import img_to_array
+#for loading the image
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.utils import to_categorical
+#for digitizing lables
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
@@ -20,45 +23,53 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-# initialize the initial learning rate, number of epochs to train for,
-# and batch size
-INIT_LR = 1e-4
-EPOCHS = 20
-BS = 32
+
 
 DIRECTORY = r"C:\Mask Detection\CODE\Face-Mask-Detection-master\dataset"
+#classification lables
 CATEGORIES = ["with_mask", "without_mask"]
 
-# grab the list of images in our dataset directory, then initialize
-# the list of data (i.e., images) and class images
-print("[INFO] loading images...")
 
+#arrays of the images will be added here
 data = []
+
 labels = []
 
+#going through both the classes in the categories ie. with mask and then without mask
 for category in CATEGORIES:
+	#merging the categories into the directory
     path = os.path.join(DIRECTORY, category)
+	#going through the list
     for img in os.listdir(path):
+	#further adding img in the path as image path
     	img_path = os.path.join(path, img)
+	#loading image inside the image path via load_img() in keras.preprocessing.image
     	image = load_img(img_path, target_size=(224, 224))
+	#img_to_array() in keras.preprocessing.image
     	image = img_to_array(image)
+	#for feeding into MobileNet CNN
     	image = preprocess_input(image)
 
     	data.append(image)
     	labels.append(category)
 
-# perform one-hot encoding on the labels
+# label needs to be digitized now using Sklearn.preprocessing()
 lb = LabelBinarizer()
 labels = lb.fit_transform(labels)
 labels = to_categorical(labels)
-
+#converting into arrays
 data = np.array(data, dtype="float32")
 labels = np.array(labels)
-
+#spliting traing and testing
 (trainX, testX, trainY, testY) = train_test_split(data, labels,
 	test_size=0.20, stratify=labels, random_state=42)
+#LR 
+INIT_LR = 1e-4
+EPOCHS = 20
+#batchsize
+BS = 32
 
-# construct the training image generator for data augmentation
+# this is for creating variations in images 
 aug = ImageDataGenerator(
 	rotation_range=20,
 	zoom_range=0.15,
@@ -68,26 +79,29 @@ aug = ImageDataGenerator(
 	horizontal_flip=True,
 	fill_mode="nearest")
 
-# load the MobileNetV2 network, ensuring the head FC layer sets are
-# left off
+#load the CNN
+#imagenet is a pre traind weight
+#3 for RGB
 baseModel = MobileNetV2(weights="imagenet", include_top=False,
 	input_tensor=Input(shape=(224, 224, 3)))
 
-# construct the head of the model that will be placed on top of the
-# the base model
+# constructing our fully connected layer
 headModel = baseModel.output
+#pooling
 headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
 headModel = Flatten(name="flatten")(headModel)
+#128 neurons with relu which is good for images
 headModel = Dense(128, activation="relu")(headModel)
+#avoiding overfitting
 headModel = Dropout(0.5)(headModel)
+#softamax is good for binary classification
 headModel = Dense(2, activation="softmax")(headModel)
 
-# place the head FC model on top of the base model (this will become
-# the actual model we will train)
+
+#our model will be the combined CNN and FC
 model = Model(inputs=baseModel.input, outputs=headModel)
 
-# loop over all layers in the base model and freeze them so they will
-# *not* be updated during the first training process
+#only traing the FC and not the base model
 for layer in baseModel.layers:
 	layer.trainable = False
 
@@ -97,7 +111,7 @@ opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
 model.compile(loss="binary_crossentropy", optimizer=opt,
 	metrics=["accuracy"])
 
-# train the head of the network
+# training our model(FC)
 print("[INFO] training head...")
 H = model.fit(
 	aug.flow(trainX, trainY, batch_size=BS),
@@ -106,23 +120,22 @@ H = model.fit(
 	validation_steps=len(testX) // BS,
 	epochs=EPOCHS)
 
-# make predictions on the testing set
+# prediction
 print("[INFO] evaluating network...")
 predIdxs = model.predict(testX, batch_size=BS)
 
-# for each image in the testing set we need to find the index of the
-# label with corresponding largest predicted probability
+#highest probabilty label is assigned 
 predIdxs = np.argmax(predIdxs, axis=1)
 
-# show a nicely formatted classification report
+# classification report
 print(classification_report(testY.argmax(axis=1), predIdxs,
 	target_names=lb.classes_))
 
-# serialize the model to disk
+# saving our custom model as mask_detector.model 
 print("[INFO] saving mask detector model...")
 model.save("mask_detector.model", save_format="h5")
 
-# plot the training loss and accuracy
+# plotting the training loss and accuracy
 N = EPOCHS
 plt.style.use("ggplot")
 plt.figure()
