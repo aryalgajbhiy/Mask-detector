3b91d173de6f46a481c50ced2a3d20a7f09e96ab
aryalgajbhiy <85826792+aryalgajbhiy@users.noreply.github.com>
2021-06-16 00:25:25 +0530

    Update detect_mask_video.py

--- glimpse2.9.5/detect_mask_video.py
+++ glimpse2.9.5/detect_mask_video.py
@@ -1,4 +1,4 @@
-# import the necessary packages
+
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
@@ -11,7 +11,6 @@ import os
 
 def detect_and_predict_mask(frame, faceNet, maskNet):
 	# grab the dimensions of the frame and then construct a blob
-	# from it
 	(h, w) = frame.shape[:2]
 	blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
 		(104.0, 177.0, 123.0))
@@ -29,24 +28,19 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 
 	# loop over the detections
 	for i in range(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with
-		# the detection
+		# extract the confidence associated with the detection
 		confidence = detections[0, 0, i, 2]
 
-		# filter out weak detections by ensuring the confidence is
-		# greater than the minimum confidence
+		#ROI
 		if confidence > 0.5:
-			# compute the (x, y)-coordinates of the bounding box for
-			# the object
+			#construct ROI
 			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 			(startX, startY, endX, endY) = box.astype("int")
 
-			# ensure the bounding boxes fall within the dimensions of
-			# the frame
+			# ROI has to be smaller than the frame ofcourse!
 			(startX, startY) = (max(0, startX), max(0, startY))
 			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
 
-			# extract the face ROI, convert it from BGR to RGB channel
 			# ordering, resize it to 224x224, and preprocess it
 			face = frame[startY:endY, startX:endX]
 			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
@@ -54,12 +48,11 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 			face = img_to_array(face)
 			face = preprocess_input(face)
 
-			# add the face and bounding boxes to their respective
-			# lists
+			#listing
 			faces.append(face)
 			locs.append((startX, startY, endX, endY))
 
-	# only make a predictions if at least one face was detected
+	# optimize for atleast one face
 	if len(faces) > 0:
 		# for faster inference we'll make batch predictions on *all*
 		# faces at the same time rather than one-by-one predictions
@@ -68,30 +61,28 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 		preds = maskNet.predict(faces, batch_size=32)
 
 	# return a 2-tuple of the face locations and their corresponding
-	# locations
+	# prediction
 	return (locs, preds)
 
-# load our serialized face detector model from disk
+# load  detector model from disk, change the location ofcourse !
 prototxtPath = r"/Users/aryalgajbhiye/Downloads/Face-Mask-Detection-master/face_detector/deploy.prototxt"
 weightsPath = r"/Users/aryalgajbhiye/Downloads/Face-Mask-Detection-master/face_detector/res10_300x300_ssd_iter_140000.caffemodel"
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
-# load the face mask detector model from disk
+# load  mask detector model 
 maskNet = load_model("mask_detector.model")
 
 # initialize the video stream
 print("[INFO] starting video stream...")
 vs = VideoStream(src=0).start()
 
-# loop over the frames from the video stream
+# loop over each frame in the video
 while True:
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
+	
 	frame = vs.read()
 	frame = imutils.resize(frame, width=400)
 
-	# detect faces in the frame and determine if they are wearing a
-	# face mask or not
+	#detect face and classify them (mask on or off)
 	(locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
 
 	# loop over the detected face locations and their corresponding
@@ -101,8 +92,7 @@ while True:
 		(startX, startY, endX, endY) = box
 		(mask, withoutMask) = pred
 
-		# determine the class label and color we'll use to draw
-		# the bounding box and text
+		
 		label = "Mask" if mask > withoutMask else "No Mask"
 		color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
 
@@ -115,14 +105,14 @@ while True:
 			cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
 		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
 
-	# show the output frame
+	
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
 
-	# if the `q` key was pressed, break from the loop
+	#press q to end
 	if key == ord("q"):
 		break
 
-# do a bit of cleanup
+
 cv2.destroyAllWindows()
 vs.stop()
